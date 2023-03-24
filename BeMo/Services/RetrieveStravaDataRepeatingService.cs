f8156176b4fe0b51bd5bed8c0b2c74d0d8c7c6ac
Paulius Minajevas <pminajevas@alna.lt>
2023-03-24 06:33:26 +0200

    finishing up service

--- BeMo/Services/RetrieveStravaDataRepeatingService.cs
+++ BeMo/Services/RetrieveStravaDataRepeatingService.cs
@@ -52,6 +52,8 @@ namespace BeMo.Services
             {
                 var _userRepository = scope.ServiceProvider.GetRequiredService<IRepository<User>>();
 
+                var _activityRepository = scope.ServiceProvider.GetRequiredService<IRepository<Activity>>();
+
                 var users = await _userRepository.GetAllAsync();
 
                 var dataURL = StravaConstants.BaseUrl + StravaConstants.ListAthleteActivitiesEndpoint;
@@ -60,48 +62,74 @@ namespace BeMo.Services
                 {
                     if (user.AccessToken is not null)
                     {
-                        //try
-                        //{
-                        //    int activitiesRetrieved;
-                        //    var activityList = new List<Activity>();
+                        try
+                        {
+                            int activitiesRetrieved;
 
-                        //    var pageNumber = 0;
-                        //    var before = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-                        //    var after = new DateTimeOffset(user.LastActivitySync).ToUnixTimeMilliseconds();
-                        //    var per_page = StravaConstants.ActivitiesPerRequest;
+                            var pageNumber = 0;
+                            var before = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                            var after = new DateTimeOffset(user.LastActivitySync).ToUnixTimeMilliseconds();
+                            var per_page = StravaConstants.ActivitiesPerRequest;
+                            do
+                            {
+                                pageNumber++;
+                                string parameteres = $"?before={before}&after={after}&page={pageNumber}&per_page={per_page}";
 
-                        //    do
-                        //    {
-                        //        pageNumber++;
-                        //        string parameteres = $"?before={before}&after={after}&page={pageNumber}&per_page={per_page}";
+                                var result = await _httpClient.GetAsync(String.Concat(dataURL, parameteres));
 
-                        //        var result = await _httpClient.GetAsync(String.Concat(dataURL, parameteres));
+                                if (!result.IsSuccessStatusCode)
+                                {
+                                    throw new BadHttpRequestException("Couldn't retrieve activities");
+                                }
 
-                        //        if (!result.IsSuccessStatusCode)
-                        //        {
-                        //            throw new BadHttpRequestException("Couldn't retrieve activities");
-                        //        }
+                                var responseBody = await result.Content.ReadAsStringAsync();
 
-                        //        var responseBody = await result.Content.ReadAsStringAsync();
+                                var response = JsonConvert.DeserializeObject<StravaGetAthleteActivitiesResponse>(responseBody) ?? throw new JsonSerializationException("Wrong response object type retrieved");
 
-                        //        var response = JsonConvert.DeserializeObject<StravaGetAthleteActivitiesResponse>(responseBody) ?? throw new JsonSerializationException("Wrong response object type retrieved");
+                                var activities = response.activities;
 
-                        //        var activities = response.activities;
+                                activitiesRetrieved = activities.Count;
 
-                        //        activitiesRetrieved = activities.Count;
+                                foreach (StravaActivityResponse activityResponse in activities)
+                                {
+                                    var activity = new Activity();
 
+                                    switch (activityResponse.type)
+                                    {
+                                        case "Walk":
+                                            activity.ActivityType = ActivityType.Walk;
+                                            break;
+                                        case "Run":
+                                            activity.ActivityType = ActivityType.Run;
+                                            break;
+                                        case "Ride":
+                                            activity.ActivityType = ActivityType.Cycle;
+                                            break;
+                                        default:
+                                            continue;
+                                    }
 
+                                    activity.AverageSpeed = activityResponse.average_speed;
+                                    activity.Elapsed = new TimeSpan(0, 0, (int)activityResponse.elapsed_time);
+                                    activity.Distance = activityResponse.distance;
+                                    activity.ElevationGain = activityResponse.total_elevation_gain;
+                                    activity.MaxSpeed = activityResponse.max_speed;
+                                    activity.StartDate = activityResponse.start_date;
+                                    activity.EndDate = activityResponse.start_date.AddSeconds(activityResponse.elapsed_time);
 
-                        //        activityList.AddRange(activities);
-                        //    }
-                        //    while (activitiesRetrieved == StravaConstants.ActivitiesPerRequest);
+                                    await _activityRepository.InsertAsync(activity);
+                                }
+                            }
+                            while (activitiesRetrieved == StravaConstants.ActivitiesPerRequest);
 
-                        //    user.LastActivitySync = DateTimeOffset.FromUnixTimeSeconds(before); 
-                        //}
-                        //catch (Exception e)
-                        //{
-                        //    _logger.LogError(e.Message);
-                        //}
+                            user.LastActivitySync = DateTime.UtcNow;
+
+                            await _userRepository.InsertAsync(user);
+                        }
+                        catch (Exception e)
+                        {
+                            _logger.LogError(e.Message);
+                        }
                     }
                 }
             }
